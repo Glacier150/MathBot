1b820bb0395e011a7a2675846c6f5ba3a151e9a4
Lil-Mari <mf.rodriguez.uspe@gmail.com>
2020-04-06 00:58:23 +0000

    multi-functionality

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import math
 
 print('Hello there!')
+#It says to print 'hello there'
 username=input("What's your name? ")
-print("In case you didn't know, I solve quadratic equations, but a warning,", username,". At this stage in development, I can only accurately process whole numbers. As a matter of fact, during testing, I've been off by several hundreds before. Anyway, let's start.")
-RawOpB=float(input('What is the opposite of B? '))
-RawB=float(input('What is B? '))
-RawA=float(input('What is A? '))
-RawC=float(input('And lastly what is C? '))
+#It says to print 'What's your name' and takes the string input as the variable 'username'
+def QuadForm(A, B, C):
+  A=float(input('What is A '))
+  B=float(input('What is B? '))
+  C=float(input('What is C? '))
+  print('Alright give me a minute to think')
+  pre_sqrt=B**2-4*A*C
+  print('I got', pre_sqrt, 'when I multiplied 4AC and subtracted it from B squared')
+  if pre_sqrt<0:
+    print('The question has no solution')
+  else:
+    post_sqrt=math.sqrt(pre_sqrt)
+    print('I got',post_sqrt,'from the square root')
+    print('I got',(round(post_sqrt,2)),'after I rounded')
+    print('And I got',((B*-1)+(round(post_sqrt,2)))/(2*A),'and',((B*-1)-(round(post_sqrt, 2)))/(2*A),'for my final answers')
 
-print('Alright give me a minute to think')
+def add(N1, N2):
+  pass
 
-#putting a hashtag first turns the line into a comment that the program will ignore
+def sub(N1, N2):
+  pass
 
-pre_sqrt=RawB**2-4*RawA*RawC
-print('I got', pre_sqrt, 'when I multiplied 4AC and subtracted it from B squared')
-post_sqrt=math.sqrt(pre_sqrt)
-print('I got',post_sqrt,'from the square root')
-print('I got',(round(post_sqrt,1)),'after I rounded')
-print('And I got',(RawOpB+(round(post_sqrt,2))),'and',(RawOpB-(round(post_sqrt, 2))),'for my final answers')
+def div(N1, N2):
+  pass
+
+def multi(N1, N2):
+  pass
